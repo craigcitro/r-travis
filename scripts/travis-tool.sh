fd8c682bb4c9c4742344c0844d7a3287b65d70f5
Craig Citro <craigcitro@google.com>
2013-10-25 22:59:48 -0700

    Do a little tidying/normalization.
    This does two things:
     * sets indentation to 4 (egads!)
     * makes devtools use uniform, with a note that it might be made optional.

--- scripts/travis-tool.sh
+++ scripts/travis-tool.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# -*- sh-basic-offset: 4; sh-indentation: 4 -*-
 # Bootstrap an R/travis environment.
 
 set -e
@@ -6,39 +7,48 @@ set -e
 OS=$(uname -s)
 
 Bootstrap() {
-  if [ "Darwin" == "${OS}" ]; then
-    BootstrapMac
-  elif [ "Linux" == "${OS}" ]; then
-    BootstrapLinux
-  else
-    echo "Unknown OS: ${OS}"
-    exit 1
-  fi
+    if [ "Darwin" == "${OS}" ]; then
+        BootstrapMac
+    elif [ "Linux" == "${OS}" ]; then
+        BootstrapLinux
+    else
+        echo "Unknown OS: ${OS}"
+        exit 1
+    fi
 }
 
 BootstrapLinux() {
-  # Update first.
-  sudo apt-get update -qq
+    # Update first.
+    sudo apt-get update -qq
 
-  # Set up our CRAN mirror.
-  sudo add-apt-repository "deb http://cran.rstudio.com/bin/linux/ubuntu precise/"
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-  sudo apt-get update -qq
+    # Set up our CRAN mirror.
+    sudo add-apt-repository "deb http://cran.rstudio.com/bin/linux/ubuntu precise/"
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+    sudo apt-get update -qq
 
-  # Install R as well as littler.
-  sudo apt-get install r-base-dev littler
+    # Install R as well as littler.
+    sudo apt-get install r-base-dev littler
 
-  # Add user to group staff to write in /usr/local/lib/R/site-library
-  sudo adduser travis staff
+    # Add user to group staff to write in /usr/local/lib/R/site-library
+    sudo adduser travis staff
+
+    # Install devtools.
+    DevtoolsInstall
 }
 
 BootstrapMac() {
-  # TODO(craigcitro): Figure out TeX in OSX+travis.
+    # TODO(craigcitro): Figure out TeX in OSX+travis.
 
-  # Install R.
-  brew install r
+    # Install R.
+    brew install r
+
+    # Install devtools.
+    DevtoolsInstall
 }
 
+# TODO(craigcitro): Consider making this optional (based on
+# [comments](https://github.com/craigcitro/r-travis/pull/17)), and if
+# so remove the installation calls in the `Bootstrap*` functions.
 DevtoolsInstall() {
     # Install devtools.
     Rscript -e 'install.packages(c("devtools"), repos=c("http://cran.rstudio.com"))'
@@ -75,30 +85,29 @@ RInstall() {
 }
 
 GithubPackage() {
-  # An embarrassingly awful script for calling install_github from a
-  # .travis.yml.
-  #
-  # Note that bash quoting makes this annoying for any additional
-  # arguments.
+    # An embarrassingly awful script for calling install_github from a
+    # .travis.yml.
+    #
+    # Note that bash quoting makes this annoying for any additional
+    # arguments.
 
-  # Get the package name and strip it
-  PACKAGE_NAME=$1
-  shift
-  
-  # Join the remaining args.
-  ARGS=$(echo $* | sed -e 's/ /, /g')
-  if [ -n "${ARGS}" ]; then
-    ARGS=", ${ARGS}"
-  fi
+    # Get the package name and strip it
+    PACKAGE_NAME=$1
+    shift
 
-  echo "Installing package: ${PACKAGE_NAME}"
-  # Install the package.
-  Rscript -e "library(devtools); options(repos = c(CRAN = 'http://cran.rstudio.com')); install_github(\"${PACKAGE_NAME}\"${ARGS})"
+    # Join the remaining args.
+    ARGS=$(echo $* | sed -e 's/ /, /g')
+    if [ -n "${ARGS}" ]; then
+        ARGS=", ${ARGS}"
+    fi
+
+    echo "Installing package: ${PACKAGE_NAME}"
+    # Install the package.
+    Rscript -e "library(devtools); options(repos = c(CRAN = 'http://cran.rstudio.com')); install_github(\"${PACKAGE_NAME}\"${ARGS})"
 }
 
 InstallDeps() {
-    Rscript -e 'library(devtools); imports <- parse_deps(as.package(".")$imports)$name; if (length(imports) > 0) install.packages(imports, repos=c("http://cran.rstudio.com"))'
-    Rscript -e 'library(devtools); suggests <- parse_deps(as.package(".")$suggests)$name; if (length(suggests) > 0) install.packages(suggests, repos=c("http://cran.rstudio.com"))'
+    Rscript -e 'library(devtools); options(repos = c(CRAN = "http://cran.rstudio.com")); devtools:::install_deps(dependencies = TRUE)'
 }
 
 RunTests() {
