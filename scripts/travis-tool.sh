8fe4c745bea6a695d628deee95a04b9610a9d666
Craig Citro <craigcitro@google.com>
2013-11-06 01:29:48 -0800

    Make mac+latex easier.
    This makes two tweaks to Kirill's hard work:
    * Add a base set of tex packages, so that we can use this for testing out of
    the box.
    * Change the way we handle paths, so that users don't have to source anything
    in their `.travis.yml` unless they're doing something fancy (like installing
    more of their own packages).

--- scripts/travis-tool.sh
+++ scripts/travis-tool.sh
@@ -7,6 +7,14 @@ set -e
 CRAN=${CRAN:-"http://cran.rstudio.com"}
 OS=$(uname -s)
 
+# MacTeX installs in a new $PATH entry, and there's no way to force
+# the *parent* shell to source it from here. So we just manually add
+# all the entries to a location we already know to be on $PATH.
+#
+# TODO(craigcitro): Remove this once we can add `/usr/texbin` to the
+# root path.
+PATH="${PATH}:/usr/texbin"
+
 R_BUILD_ARGS=${R_BUILD_ARGS-"--no-build-vignettes"}
 R_CHECK_ARGS=${R_CHECK_ARGS-"--no-manual --as-cran"}
 
@@ -74,7 +82,7 @@ BootstrapMac() {
 }
 
 BootstrapMacOptions() {
-    if [ -n "$BOOTSTRAP_LATEX" ]; then
+    if [[ -n "$BOOTSTRAP_LATEX" ]]; then
         # TODO: Install MacTeX.pkg once there's enough disk space
         MACTEX=mactex-basic.pkg
         wget http://ctan.math.utah.edu/ctan/tex-archive/systems/mac/mactex/$MACTEX -O "/tmp/$MACTEX"
@@ -82,6 +90,11 @@ BootstrapMacOptions() {
         echo "Installing OS X binary package for MacTeX"
         sudo installer -pkg "/tmp/$MACTEX" -target /
         rm "/tmp/$MACTEX"
+        # We need a few more packages than the basic package provides; this
+        # post saved me so much pain:
+        #   https://stat.ethz.ch/pipermail/r-sig-mac/2010-May/007399.html
+        sudo tlmgr update --self
+        sudo tlmgr install inconsolata upquote courier courier-scaled helvetic
     fi
 }
 
