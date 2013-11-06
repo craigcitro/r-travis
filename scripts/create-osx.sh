b033c150cc8af5be16ff69add84be6cb58e83bdc
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-11-06 11:43:09 +0100

    hybrid

--- scripts/create-osx.sh
+++ scripts/create-osx.sh
@@ -10,7 +10,17 @@ if test -n "$(git status --porcelain)"; then
   exit 1
 fi
 
-git branch -D $OSX_BRANCH
+if [[ -n "$(git branch | egrep "^ *$OSX_BRANCH")" ]]; then
+    git checkout $OSX_BRANCH
+    if git merge $BRANCH --no-edit; then
+        git checkout $BRANCH
+        exit 0
+    fi
+    echo "WARNING: Can't merge, falling back to overwriting branch"
+    git checkout $BRANCH
+    git branch -D $OSX_BRANCH
+fi
+
 git checkout -b $OSX_BRANCH
 sed -i -r 's/^language: c/language: objective-c/' .travis.yml
 git add .travis.yml
