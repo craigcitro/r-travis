19b99a5e3143871314f0383ae0b16d6ce5b5823f
Craig Citro <craigcitro@google.com>
2013-11-06 02:26:28 -0800

    Make Dirk's suggested edits to build/check defaults.
    Fixes #64.
    Closes #65.

--- scripts/travis-tool.sh
+++ scripts/travis-tool.sh
@@ -17,8 +17,8 @@ OS=$(uname -s)
 # root path.
 PATH="${PATH}:/usr/texbin"
 
-R_BUILD_ARGS=${R_BUILD_ARGS-"--no-build-vignettes"}
-R_CHECK_ARGS=${R_CHECK_ARGS-"--no-manual --as-cran"}
+R_BUILD_ARGS=${R_BUILD_ARGS-"--no-build-vignettes --no-manual"}
+R_CHECK_ARGS=${R_CHECK_ARGS-"--no-build-vignettes --no-manual --as-cran"}
 
 Bootstrap() {
     if [[ "Darwin" == "${OS}" ]]; then
