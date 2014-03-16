23944fb473d225f7fe4aab6ed55f3a26c2281d35
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-03-16 22:58:22 +0100

    avoid incoming checks by running R CMD check with  _R_CHECK_CRAN_INCOMING_=FALSE

--- scripts/travis-tool.sh
+++ scripts/travis-tool.sh
@@ -202,7 +202,8 @@ RunTests() {
     FILE=$(ls -1t *.tar.gz | head -n 1)
 
     echo "Testing with: R CMD check \"${FILE}\" ${R_CHECK_ARGS}"
-    R CMD check "${FILE}" ${R_CHECK_ARGS}
+    echo "(CRAN incoming checks are off)"
+    _R_CHECK_CRAN_INCOMING_=FALSE R CMD check "${FILE}" ${R_CHECK_ARGS}
 
     if [[ -n "${WARNINGS_ARE_ERRORS}" ]]; then
         if DumpLogsByExtension "00check.log" | grep -q WARNING; then
